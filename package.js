7867e0ea0506c02c95302bf62894ccf1bd3c995a
d2tstudio <derry@d2tstudio.com>
2015-01-16 16:49:07 +0100

    Add version numbers to apis

--- package.js
+++ package.js
@@ -5,13 +5,13 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('underscore', 'client');
-  api.use('templating', 'client');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
+  api.use('oauth2@1.1.2', ['client', 'server']);
+  api.use('oauth@1.1.3', ['client', 'server']);
+  api.use('http@1.0.9', ['server']);
+  api.use('underscore@1.0.2', 'client');
+  api.use('templating@1.0.10', 'client');
+  api.use('random@1.0.2', 'client');
+  api.use('service-configuration@1.0.3', ['client', 'server']);
 
   api.export('Dribbble');
 
